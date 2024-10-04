28631db6a9724bee9b66f68bb2a637ade99d029f
João Gabriel <142173068+JohnnyBortoni@users.noreply.github.com>
2024-10-03 21:13:19 -0300

    add javasc

---
+++ progamanation.js
@@ -0,0 +1,18 @@
+document.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll("haid]"); // Seções do conteúdo
+    const menuItems = document.querySelectorAll(".steps li"); // Itens de navegação
+
+    // Função para alterar a cor dos círculos
+    sections.forEach((section, index) => {
+        const rect = section.getBoundingClientRect(); // Posição da seção na viewport
+
+        // Se o topo da seção está visível na tela
+        if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+            // Altera a cor do círculo para #00FF9F se a seção estiver visível
+            menuItems[index].style.setProperty('--circle-color', '#00FF9F');
+        } else {
+            // Caso contrário, volta para a cor branca
+            menuItems[index].style.setProperty('--circle-color', 'white');
+        }
+    });
+});
